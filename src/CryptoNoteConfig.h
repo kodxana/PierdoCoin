ee2f01d1a83466bc3942637d302f102f387cc38f
kodxana <romanrex19@gmail.com>
2017-12-22 13:10:49 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  94.246.161.168:17236",
-  94.246.161.168:17236",
+  109.196.82.9:17236",
+  109.196.82.9:17236",
 };
 
 struct CheckpointData {
